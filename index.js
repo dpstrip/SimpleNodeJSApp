6dd97dc11f31aa1cda3e9f9fe42359342a2b9bf6
David Stripeik <dpstrip@gmail.com>
2023-12-19 08:25:05 -0600

    added get ip, gets vip address

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var ip = require("ip");
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World</h1>');
+    let msg = '<h1>Hello World</h1><p> the ip address is: ' + ip.address();
+    res.end(msg);
 }).listen(3080);
